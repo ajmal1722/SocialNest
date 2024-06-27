d63063659f01470b8d4fa46335a81baf36e8566f
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-27 19:48:57 +0530

    connected to mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
+import connectDB from './database/connection.js';
 
 const app = express()
+
+connectDB()
 
 const PORT = process.env.PORT || 3000
 
