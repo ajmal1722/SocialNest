9c24ea33eb80d697be3924ecfe47fa5c517a79b3
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-29 16:43:36 +0530

    react hook form

--- frontend/src/components/authentication/UserLogin.jsx
+++ frontend/src/components/authentication/UserLogin.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
 import { FaRegUser, FaLock } from "react-icons/fa";
 
 const UserLogin = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm({
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    });
+
+    const userLogin = data => console.log(data)
+
     return (
         <div className="flex justify-center items-center h-screen">
             <div className=' pt-10 pb-6 px-6 max-w-md min-w-80 shadow-2xl shadow-gray-500/40'>
                 <div className="textcenter text-3xl my-8 text-center font-semibold">
                     Login
                 </div>
-                <form >
+                <form onSubmit={handleSubmit(userLogin)} >
                     <div className='flex min-w-72 items-center border-b-2 text-gray-400 mb-6 '>
                         <FaRegUser />
                         <input 
                             type="text"
-                            name='email'
+                            {...register('email', { required: true })}
                             placeholder='Username or email'
                             className='p-3 w-full focus:outline-none'
                         />
                     </div>
+                        {errors.email && <p className='error-message'>This field is required</p>}
                     <div className='flex min-w-72 items-center border-b-2 text-gray-400 mb-6 '>
                         <FaLock />
                         <input 
                             type="password"
-                            name='password'
+                            {...register('password', { required: true })}
                             placeholder='Password'
                             className='p-3 w-full focus:outline-none'
                         />
                     </div>
-                    <button className='bg-blue-400 hover:bg-blue-500 text-white w-full rounded-full p-2 '>
+                    {errors.password && <p className='error-message'>This field is required</p>}
+                    <button type='submit' className='bg-blue-400 hover:bg-blue-500 text-white w-full rounded-full p-2 '>
                         Login
                     </button>
                 </form>
