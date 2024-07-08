32747e92c32438b2d4384476df12c833265a3dd2
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-07-08 17:01:16 +0530

    set active link style

--- frontend/src/components/shared/SideBar.jsx
+++ frontend/src/components/shared/SideBar.jsx
@@ -2,6 +2,10 @@ import sidebarData from "../../data/sidebarData";
 import { NavLink } from "react-router-dom";
 
 const SideBar = () => {
+    const linkClass = ({ isActive }) => (
+        isActive ? 'flex items-center gap-4 p-2 hover:bg-gray-300 dark:hover:bg-secondary-dark lg:px-12 md:px-8 font-semibold underline'
+        : 'flex items-center gap-4 p-2 hover:bg-gray-300 dark:hover:bg-secondary-dark lg:px-12 md:px-8 '
+    )
     return (
         <div className='sidebar col-span-2 hidden sm:block border-r'>
             <div className='bg-primary-light dark:bg-primary-dark h-full py-8 text-xl text-primary-dark dark:text-primary-light'>
@@ -9,7 +13,7 @@ const SideBar = () => {
                     sidebarData.map((item, index) => {
                         return (
                             <div key={index}>
-                                <NavLink to={item.url} className="flex items-center gap-4 p-2 hover:bg-gray-00 dark:hover:bg-secondary-dark lg:px-12 md:px-8 " activeClassName="bg-emerald-600 font-extrabold">
+                                <NavLink to={item.url} className={linkClass} >
                                     <item.icon  />
                                     <h1 className="">{item.name}</h1>
                                 </NavLink>
