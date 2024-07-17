641b606ada116fccd33fca0f33f6c927dfbf9cde
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-07-17 19:10:11 +0530

    sigle post Ui completed

--- frontend/src/components/reusable/SinglePost.jsx
+++ frontend/src/components/reusable/SinglePost.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const SinglePost = ({ post }) => {
     return (
-        <div className='mx-auto border max-w-lg my-2'>
-            <p>
-                { post.blogContent }
+        <div className='mx-auto border max-w-lg my-2 p-3 rounded-lg text-primary-dark dark:text-primary-light'>
+            <div className='flex items-center gap-3 h-14 p-2'>
+                <img
+                    src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg" alt=""
+                    className='rounded-full max-w-10'
+                />
+                <h1 className='text-lg'>
+                    Username
+                </h1>
+                <p className='text-xs'>
+                    3 hr ago
+                </p>
+            </div>
+            <h1 className='text-xl font-semibold my-2'>
+                blog title
+            </h1>
+            <p className='text-gray-700 dark:text-gray-400'>
+                {post.blogContent}
             </p>
         </div>
     )
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -36,7 +36,7 @@ const ProfilePage = () => {
     }
 
     return (
-        <div className='min-h-[90vh] md:col-span-8 col-span-10 text-center px-4 lg:px-8'>
+        <div className='min-h-[90vh] md:col-span-8 col-span-10 px-4 lg:px-8 mt-4 mb-16 md:mb-1'>
             <ProfileImage />
             <LogoutButton navigate={navigate} />
             <PostListingLinks setActiveLink={setActiveLink} />
