bb07461ff5b7bae87e3af742fd454382219128de
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-07-22 15:33:03 +0530

    password changed successfully

--- backend/controllers/userAuthController.js
+++ backend/controllers/userAuthController.js
@@ -271,7 +271,22 @@ const generateOtp = async (req, res) => {
 
 const changePassword = async (req, res) => {
     try {
-        console.log(req.body);
+        const { email, password } = req.body;
+
+        const user = await Users.findOne({ email })
+
+        if (!user) {
+            return res.status(404).json({ error: 'User does not exist' });
+        }
+
+        // Hash the new password
+        const hashedPassword = await bcrypt.hash(password, 8);
+
+        // Update the user's password
+        user.password = hashedPassword;
+        await user.save();
+
+        res.status(200).json({ message: 'Password changed successfully' });
     } catch (error) {
         console.error('Error during OTP generation:', error);
         res.status(500).json({ error: 'Internal server error' });
