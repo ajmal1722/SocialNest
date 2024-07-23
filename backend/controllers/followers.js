a1283b3e7badf700ae5968ed34dba235c451a92b
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-07-23 19:36:39 +0530

    created followUser api

---
+++ backend/controllers/followers.js
@@ -0,0 +1,41 @@
+import Users from '../models/userSchema';
+
+export const fetchSuggestions = async (req, res) => {
+    try {
+        const userId = req.user;
+
+        const user = await Users.findById(userId);
+
+        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const followUser = async (req, res) => {
+    try {
+        const userId = req.user;
+        const userToFollowId = req.params.id;
+
+        const user = await Users.findById(userId);
+        const userToFollow = await Users.findById(userToFollowId);
+
+        if (!userToFollow) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
+        if (user.following.includes(userToFollowId)) {
+            return res.status(400).json({ message: 'You are already following this user' });
+        }
+
+        user.following.push(userToFollowId);
+        userToFollow.followers.push(userId);
+
+        await user.save()
+        await userToFollow.save()
+
+        res.status(200).json({ message: 'User followed successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
