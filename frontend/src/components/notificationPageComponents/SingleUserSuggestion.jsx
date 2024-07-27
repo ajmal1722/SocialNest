b45ef869319fbe129492ec7522aa8340f2ea1697
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-07-27 16:42:03 +0530

    created separate component for follow and unfollow button

--- frontend/src/components/notificationPageComponents/SingleUserSuggestion.jsx
+++ frontend/src/components/notificationPageComponents/SingleUserSuggestion.jsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { followUserApi, unoFllowUserApi } from "../../utils/api/follow_api";
-import { follow_user, unfollow_user } from "../../redux/slices/authSlice";
+import FollowAndUnfollowButton from "../reusable/FollowAndUnfollowButton";
 
 const SingleUserSuggestion = ({ data }) => {
-    const [showUnfollowButton, setShowUnfollowButton] = useState(false)
-    const dispatch = useDispatch();
-
-    const followUser = async (id) => {
-        const response = await followUserApi(id)
-        dispatch(follow_user(response));
-        setShowUnfollowButton(true)
-    }
-
-    const unFollowUser = async (id) => {
-        const response = await unoFllowUserApi(id)
-        dispatch(unfollow_user(response));
-        setShowUnfollowButton(false);
-    }
-
+   
     return (
         <div className='flex justify-between gap-2 border p-2 rounded-lg m-2'>
             <div className='flex gap-2 text-primary-dark dark:text-primary-light'>
@@ -37,18 +20,7 @@ const SingleUserSuggestion = ({ data }) => {
                 </h2>
             </div>
             </div>
-            {
-                showUnfollowButton ? (
-                    <button onClick={() => unFollowUser(data._id)} className='mr-4 text-base font-semibold hover:text-blue-500 text-primary-dark dark:text-primary-light'>
-                        Unfollow
-                    </button>
-                ) : (
-                    <button onClick={() => followUser(data._id)} className='mr-4 text-base font-semibold text-blue-500 hover:text-primary-dark hover:dark:text-primary-light'>
-                        Follow
-                    </button>
-                )
-            }
-            
+            <FollowAndUnfollowButton data={data} />
         </div>
     )
 }
---
+++ frontend/src/components/reusable/FollowAndUnfollowButton.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { followUserApi, unoFllowUserApi } from "../../utils/api/follow_api";
+import { follow_user, unfollow_user } from "../../redux/slices/authSlice";
+
+const FollowAndUnfollowButton = ({ data }) => {const [showUnfollowButton, setShowUnfollowButton] = useState(false)
+    const dispatch = useDispatch();
+
+    const followUser = async (id) => {
+        const response = await followUserApi(id)
+        dispatch(follow_user(response));
+        setShowUnfollowButton(true)
+    }
+
+    const unFollowUser = async (id) => {
+        const response = await unoFllowUserApi(id)
+        dispatch(unfollow_user(response));
+        setShowUnfollowButton(false);
+    }
+    
+    return (
+        <div>
+            {
+                showUnfollowButton ? (
+                    <button onClick={() => unFollowUser(data._id)} className='mr-4 text-base font-semibold hover:text-blue-500 text-primary-dark dark:text-primary-light'>
+                        Unfollow
+                    </button>
+                ) : (
+                    <button onClick={() => followUser(data._id)} className='mr-4 text-base font-semibold text-blue-500 hover:text-primary-dark hover:dark:text-primary-light'>
+                        Follow
+                    </button>
+                )
+            }
+        </div>
+    )
+}
+
+export default FollowAndUnfollowButton
