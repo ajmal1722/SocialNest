344225e1d9599a05f0cb8c4a2b00177bd3eba5c5
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-08-01 19:19:51 +0530

    updated profile page style

--- frontend/src/components/profileComponents/ProfileImage.jsx
+++ frontend/src/components/profileComponents/ProfileImage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProfileImage = () => {
   return (
-        <div >
+        <div className='mt-10 flex justify-center' >
             <img 
                 src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg" alt="" 
-                className='rounded-full max-w-48'
+                className='rounded-full w-auto max-w-52'
             />
         </div>
   )
--- frontend/src/components/profileComponents/ProfileInfo.jsx
+++ frontend/src/components/profileComponents/ProfileInfo.jsx
@@ -33,61 +33,69 @@ const ProfileInfo = () => {
     console.log('date:', userInfo);
 
     return (
-        <div >
-            <div className='flex my-4 gap-2 '>
-                <h1 className='text-2xl font-semibold'>
-                    {userInfo.name}
-                </h1>
-                <button className='text-lg px-6 py-1 ml-8 rounded-lg bg-ternary-dark dark:bg-secondary-dark text-white flex items-center gap-2'>
-                    Edit Profile
-                    <LiaUserEditSolid className="text-2xl " />
-                </button>
-                <LogoutButton />
-            </div>
-            <h1 className='font-semibold mb-4'>
-                <span className='font-bold text-2xl m-1'>@</span>
-                {userInfo.username}
-            </h1>
-            <div className='flex text-center gap-6 my-4'>
-                <div className='cursor-pointer'>
-                    <h1 className='text-2xl font-bold'>
-                        0
+        <div className="flex justify-center mt-5">
+            <div >
+                <div className='md:flex my-4 gap-2 items-center'>
+                    <h1 className='text-2xl font-semibold mb-3 text-center mt-2'>
+                        {userInfo.name}
                     </h1>
-                    <h1>
-                        Posts
-                    </h1>
+                    <div className="flex gap-4">
+                        <button className='text-lg md:px-6 px-3 py-1 md:ml-8 rounded-lg bg-ternary-dark dark:bg-secondary-dark text-white flex items-center gap-2'>
+                            Edit Profile
+                            <LiaUserEditSolid className="text-2xl " />
+                        </button>
+                        <LogoutButton />
+                    </div>
                 </div>
-                <div onClick={showFollowers} className='cursor-pointer'>
-                    <h1 className='text-2xl font-bold'>
-                        {followers.length}
-                    </h1>
-                    <h1>
-                        Followers
-                    </h1>
-                </div>
-                <div onClick={showFollowing} className='cursor-pointer'>
-                    <h1 className='text-2xl font-bold'>
-                        {following.length}
-                    </h1>
-                    <h1>
-                        Following
-                    </h1>
-                </div>
-            </div>
-            {
-                userInfo.createdAt &&
-                <div className="flex items-center gap-1">
-                    Member since
-                    <DateFormatter date={userInfo.createdAt} />
+                <div className="flex justify-center md:block">
+                    <div className="">
+                        <h1 className='font-semibold mb-4 text-center md:text-start'>
+                            <span className='font-bold text-2xl m-1'>@</span>
+                            {userInfo.username}
+                        </h1>
+                        <div className='flex gap-6 my-4 text-center '>
+                            <div className='cursor-pointer'>
+                                <h1 className='text-2xl font-bold'>
+                                    0
+                                </h1>
+                                <h1>
+                                    Posts
+                                </h1>
+                            </div>
+                            <div onClick={showFollowers} className='cursor-pointer'>
+                                <h1 className='text-2xl font-bold'>
+                                    {followers.length}
+                                </h1>
+                                <h1>
+                                    Followers
+                                </h1>
+                            </div>
+                            <div onClick={showFollowing} className='cursor-pointer'>
+                                <h1 className='text-2xl font-bold'>
+                                    {following.length}
+                                </h1>
+                                <h1>
+                                    Following
+                                </h1>
+                            </div>
+                        </div>
+                        {
+                            userInfo.createdAt &&
+                            <div className="flex items-center gap-1 md:justify-normal justify-center">
+                                Member since
+                                <DateFormatter date={userInfo.createdAt} />
 
+                            </div>
+                        }
+                    </div>
                 </div>
-            }
 
-            <ContentDisplayingModal
-                isVisible={isModalVisible}
-                onClose={closeModal}
-                content={modalContent}
-            />
+                <ContentDisplayingModal
+                    isVisible={isModalVisible}
+                    onClose={closeModal}
+                    content={modalContent}
+                />
+            </div>
         </div>
     )
 }
