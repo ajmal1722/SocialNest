91bf07467fe4c250b90c62696cfd1b60fcea9241
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-08-10 10:47:20 +0530

    sample editProfiel ui created

--- frontend/src/pages/EditProfilePage.jsx
+++ frontend/src/pages/EditProfilePage.jsx
@@ -1,4 +1,8 @@
 import { useForm, FormProvider } from 'react-hook-form';
+import Input from 'antd/es/input/Input';
+import TextInput from '../components/reusable/TextInput';
+import SubmitButton from '../components/reusable/SubmitButton';
+
 
 const EditProfilePage = () => {
     const methods = useForm();
@@ -9,7 +13,7 @@ const EditProfilePage = () => {
         // console.log('Post Data:', postData);
         if (initialData) {
             console.log('updata:', postData);
-            
+
             // If initialData is provided, update the post
             await updatePostApi(initialData._id, postData);
         } else {
@@ -19,35 +23,48 @@ const EditProfilePage = () => {
 
         reset()
     };
+
+    const nameValidation = {
+        required: 'This field is required',
+        minLength: {
+            value: 3,
+            message: 'Name must be at least 3 characters'
+        },
+    };
+
     return (
         <div className='min-h-[90vh] md:col-span-8 col-span-10 px-4 lg:px-8 mt-4 mb-16 md:mb-1'>
             <h1 className='text-center'>
                 Edit Profile
             </h1>
-            {/* <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit(submitTextPost)}>
-                    <TextInput
-                        name="caption"
-                        validation={titleValidation}
-                        maxLength={35}
-                        placeholder="Type your blog title here"
-                        className="bg-primary-light dark:bg-primary-dark text-ternary-dark dark:text-primary-light my-3"
-                    />
-                    {contentType === 'Blog' ? (
+            <div className='lg:w-9/12 w-full'>
+                <FormProvider {...methods}>
+                    <form onSubmit={methods.handleSubmit(submitTextPost)}>
+                        <div className='flex border p-3 my-4 mt-8 rounded-lg'>
+                            <img  alt="" 
+                                src=""
+                                className='rounded-full max-h-28'
+                            />
+                        </div>
                         <TextInput
-                            name="blogContent"
-                            validation={contentValidation}
-                            maxLength={300}
+                            name="name"
+                            validation={nameValidation}
+                            maxLength={30}
+                            placeholder="Enter your name"
+                            className="bg-primary-light dark:bg-primary-dark text-ternary-dark dark:text-primary-light my-3 "
+                        />
+                        <TextInput
+                            name="bio"
+                            maxLength={150}
                             placeholder="Type your message here"
                             className="p-2 h-32"
                         />
-                    ) : <ImageInput onFilesChange={setFiles} />
-                    }
-                    <div className="flex justify-end my-6">
-                        <SubmitButton content={'Post'} />
-                    </div>
-                </form>
-            </FormProvider> */}
+                        <div className="flex justify-end my-6">
+                            <SubmitButton content={'Post'} />
+                        </div>
+                    </form>
+                </FormProvider>
+            </div>
         </div>
     )
 }
