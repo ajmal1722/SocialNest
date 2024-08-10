0a314348bd0fe554352c01ac845ceda23052a861
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-08-10 18:12:27 +0530

    updated the ui

--- frontend/src/components/archiveComponents/SingleBlogCard.jsx
+++ frontend/src/components/archiveComponents/SingleBlogCard.jsx
@@ -2,17 +2,23 @@ import { Card, Col } from 'antd';
 
 const SingleBlogCard = ({ post }) => {
     return (
-        <Col span={8}>
-            <Card 
+        <Col
+            xs={24}  // Full width on extra small screens
+            sm={12}  // Half width on small screens
+            md={12}   // One-third width on medium screens
+            lg={8}   // One-third width on large screens
+            className='my-3'
+        >
+            <Card
                 title={<span className="dark:text-white font-bold">{post.caption}</span>}
-                bordered={false} 
+                bordered={false}
                 extra={<a href="#">More</a>}
                 className='min-h-80 lg:min-h-64 bg-white shadow-2xl dark:bg-ternary-dark text-primary-dark dark:text-primary-light hover:scale-105'
             >
                 {post.blogContent}
             </Card>
         </Col>
-    )
+    );
 }
 
-export default SingleBlogCard
+export default SingleBlogCard;
