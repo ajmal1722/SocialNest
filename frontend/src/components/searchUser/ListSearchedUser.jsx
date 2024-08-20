758520dbfdaebf4c479f7a462d4b1fc80d46de28
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-08-20 14:27:39 +0530

    user search updation

--- frontend/src/components/searchUser/ListSearchedUser.jsx
+++ frontend/src/components/searchUser/ListSearchedUser.jsx
@@ -1,51 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { Avatar, List, message } from 'antd';
+import React from 'react';
+import { Avatar, List } from 'antd';
 import { Link } from 'react-router-dom';
 import VirtualList from 'rc-virtual-list';
 
 const ContainerHeight = 400;
 
 const ListSearchedUser = ({ userData }) => {
-
-    const appendData = () => {
-        
-    };
-
-    useEffect(() => {
-        appendData();
-    }, []);
-
-    const onScroll = (e) => {
-        // Refer to: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#problems_and_solutions
-        // if (Math.abs(e.currentTarget.scrollHeight - e.currentTarget.scrollTop - ContainerHeight) <= 1) {
-        //     appendData();
-        // }
-    };
-
     return (
-        <List className='w-full sm:w-10/12 lg:w-4/6 px-4'>
-            <VirtualList
-                data={userData}
-                height={ContainerHeight}
-                itemHeight={47}
-                itemKey="email"
-                onScroll={onScroll}
-            >
-                {(item) => (
-                    <List.Item key={item.email}>
-                        <Link to={`/user/${item._id}`}>
-                        <List.Item.Meta
-                            avatar={<Avatar src={item.profilePicture || 'https://via.placeholder.com/150'} />}
-                            title={item.username}
-                            description={item.email}
-                        />
-                        <div>{item.bio}</div>
-                        </Link>
-                    </List.Item>
-                )}
-            </VirtualList>
-        </List>
-    )
+        <div className='w-full sm:w-10/12 lg:w-4/6 px-4'>
+            {!userData ? (
+                <div className='text-center text-gray-500 dark:text-gray-400 py-10'>
+                    No results found
+                </div>
+            ) : (
+                <List>
+                    <VirtualList
+                        data={userData}
+                        height={ContainerHeight}
+                        itemHeight={47}
+                        itemKey="email"
+                    >
+                        {(item) => (
+                            <List.Item key={item.email}>
+                                <Link to={`/user/${item._id}`}>
+                                    <List.Item.Meta
+                                        avatar={<Avatar src={item.profilePicture || 'https://via.placeholder.com/150'} />}
+                                        title={item.username}
+                                        description={item.email}
+                                    />
+                                    <div>{item.bio}</div>
+                                </Link>
+                            </List.Item>
+                        )}
+                    </VirtualList>
+                </List>
+            )}
+        </div>
+    );
 }
 
-export default ListSearchedUser
+export default ListSearchedUser;
