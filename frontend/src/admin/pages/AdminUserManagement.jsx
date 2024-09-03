008939b7bd4b0186934d7c6a3e6f1c7116740ecf
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-09-03 19:09:25 +0530

    sample user details page

--- frontend/src/admin/pages/AdminUserManagement.jsx
+++ frontend/src/admin/pages/AdminUserManagement.jsx
@@ -1,34 +1,11 @@
 import { useState, useEffect } from 'react';
 import { Space, Table, Tag, Avatar, Image, Button } from 'antd';
 import { fetchAllUsersApi } from '../../utils/api/admin_api';
-
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Username',
-        dataIndex: 'username',
-        key: 'username',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-    },
-    {
-        title: 'Action',
-        dataIndex: 'reasonForReport',
-        key: 'reason',
-    }
-]
-
-const data = [{}]
+import ReusableModal from '../../components/reusable/ReusableModal';
 
 const AdminUserManagement = () => {
     const [users, setUsers] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -43,8 +20,54 @@ const AdminUserManagement = () => {
         fetchUsers();
     }, []);
 
+    const banUser = async (id) => {
+        console.log(id)
+    }
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Username',
+            dataIndex: 'username',
+            key: 'username',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (record) => (
+                <Space>
+                    <Button 
+                        className='bg-sky-500 text-white font-semibold'
+                        onClick={() => setShowModal(true)}
+                    >
+                        View User
+                    </Button>
+                    <Button onClick={() => banUser(record._id)}>
+                        Ban User
+                    </Button>
+                </Space>
+            )
+        }
+    ]
+
     return (
-        <Table columns={columns} dataSource={users} />
+        <>
+            <Table columns={columns} dataSource={users} />
+            <ReusableModal 
+                isVisible={showModal}
+                onClose={() => setShowModal(false)}
+                // Content={}
+            />
+        </>
     )
 }
 
