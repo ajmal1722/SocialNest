c770e8e76fadb808c6e16f8c8db146594280f963
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-09-17 18:42:31 +0530

    update on dashboard

---
+++ frontend/src/admin/components/reusable/DashboardBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardBox = () => {
+    return (
+        <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4">Total Users</h2>
+            <h2 className="text-2xl font-semibold mb-4">Total Users</h2>
+        </div>
+    )
+}
+
+export default DashboardBox
--- frontend/src/admin/pages/AdminDashboard.jsx
+++ frontend/src/admin/pages/AdminDashboard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import DashboardBox from "../components/reusable/DashboardBox";
 
 const AdminDashboard = () => {
     return (
-        <div className="bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">Welcome to the Admin Dashboard</h2>
-            {/* Add more dashboard content here */}
+        <div className="">
+            <DashboardBox />
+            <DashboardBox />
         </div>
     );
 };
