a973c6cede6b779914a34f363b4bd7fdfd8af175
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-09-28 18:20:06 +0530

    in main layout socket emitted with userId

--- backend/sockets/chatSocket.js
+++ backend/sockets/chatSocket.js
@@ -1,5 +1,12 @@
+const users = {} // Object to map userId to socketId
 
 const chatSocket = (io, socket) => {
+    // When a user connects, store their userId and socketId as key value pairs
+    socket.on('userConnected', (userId) => {
+        users[userId] = socket.id; // Store the socket id for the connected user
+        console.log(`User ${userId} connected with socket ID: ${socket.id}`);
+    });
+    
     // Listen for chat message events
     socket.on('chatMessage', (message) => {
         console.log('New message received:', message);
--- frontend/src/layoutes/MainLayout.jsx
+++ frontend/src/layoutes/MainLayout.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { io } from 'socket.io-client'
 import Navbar from '../components/shared/Navbar';
 import LeftSideBar from "../components/shared/SideBar";
 import MenuFooter from "../components/shared/MenuFooter";
 
 const MainLayout = () => {
-  const  { userInfo }  = useSelector(state => state.auth)
+  const socket = io('http://localhost:8000')
+  const { userInfo } = useSelector(state => state.auth);
 
+  useEffect(() => {
+    socket.emit('userConnected', userInfo._id);
+
+    // Clean up the connection when the component unmounts
+    return () => {
+      socket.off('chatMessage');
+      socket.emit('userDisconnected', userInfo._id);
+    };
+  }, [])
   return (
     <>
       <Navbar />
