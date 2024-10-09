8a4ae60df4ff7b2319962834237370570ca6f092
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-10-09 14:34:07 +0530

    added archive button in profile page

--- frontend/src/components/profileComponents/ProfileInfo.jsx
+++ frontend/src/components/profileComponents/ProfileInfo.jsx
@@ -58,33 +58,41 @@ const ProfileInfo = ({ profileData, posts }) => {
                     <h1 className='text-2xl font-semibold mb-3 text-center mt-2'>
                         {name}
                     </h1>
-                    {userInfo._id === userId ?  
-                    <div className="flex gap-4">
-                        <Link to='/edit-profile'>
-                            <button className='text-lg md:px-6 px-3 py-1 md:ml-8 rounded-lg bg-ternary-dark dark:bg-secondary-dark text-white flex items-center gap-2'>
-                                Edit Profile
-                                <LiaUserEditSolid className="text-2xl" />
-                            </button>
-                        </Link>
-                        <LogoutButton />
-                    </div> : (
-                        <div className="flex gap-2 justify-center px-5 text-white">
-                            <FollowAndUnfollowButton 
-                                data={profileData}
-                                followButtonStyle={'bg-blue-500 text-white font-semibold px-10 p-1 rounded-lg'}
-                                unFollowButtonStyle={'bg-ternary-dark dark:bg-secondary-dark text-white font-semibold px-10 p-1 rounded-lg '}
-                            />
-                            <BlockAndUnBlockButton data={profileData} />
-                        </div>
-                )}
+                    {userInfo._id === userId ?
+                        <div className="flex gap-4">
+                            <Link to='/edit-profile'>
+                                <button className='text-lg md:px-6 px-3 py-1 md:ml-8 rounded-lg bg-ternary-dark dark:bg-secondary-dark text-white flex items-center gap-2'>
+                                    Edit Profile
+                                    <LiaUserEditSolid className="text-2xl" />
+                                </button>
+                            </Link>
+                            <LogoutButton />
+                        </div> : (
+                            <div className="flex gap-2 justify-center px-5 text-white">
+                                <FollowAndUnfollowButton
+                                    data={profileData}
+                                    followButtonStyle={'bg-blue-500 text-white font-semibold px-10 p-1 rounded-lg'}
+                                    unFollowButtonStyle={'bg-ternary-dark dark:bg-secondary-dark text-white font-semibold px-10 p-1 rounded-lg '}
+                                />
+                                <BlockAndUnBlockButton data={profileData} />
+                            </div>
+                        )}
                 </div>
                 <div className="flex justify-center md:block">
                     <div>
-                        <h1 className='font-semibold mb-4 text-center md:text-start'>
+                        <h1 className='font-semibold mb-4 text-center md:text-start mt-1'>
                             <span className='font-bold text-2xl m-1'>@</span>
                             {username}
                         </h1>
-                        <div className='flex gap-6 my-4 text-center'>
+                        <div className="flex items-center md:justify-start justify-center gap-3 mb-1">
+                            {userInfo._id === userId &&
+                                <Link to='/archive'>
+                                    <button className='px-3 p-1 rounded-lg bg-ternary-dark dark:bg-secondary-dark text-white flex items-center gap-2'>
+                                        View Archive
+                                    </button>
+                                </Link>}
+                        </div>
+                        <div className='flex gap-6 mb-4 text-center'>
                             <div className='cursor-pointer'>
                                 <h1 className='text-2xl font-bold'>
                                     {posts.length}
