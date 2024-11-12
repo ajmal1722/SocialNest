b425ffad04b00363eac523425fdb3f79818d2441
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-11-12 16:21:31 +0530

    cors origin updated

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express(); // Create Express app
 
 // CORS options to allow requests from specific origin
 const corsOptions = {
-    origin: 'http://localhost:5173', // Allow requests from this frontend domain
+    origin: ['http://localhost:5173', 'https://social-nest-alpha.vercel.app'], // Allow requests from this frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Include cookies in requests
 };
